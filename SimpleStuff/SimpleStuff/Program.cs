89d87c8fb9a77135a0ce034679354950eca8dd2d
Matthew Vincent <themevinv@gmail.com>
2018-09-28 21:15:02 -0400

    The first commit.

---
+++ SimpleStuff/SimpleStuff/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+        }
+    }
+}
